86709338f72a54e6470be395cce2ad061e72bb0a
Mahdisaadaat <mahdisaadaat@gmail.com>
2022-09-16 17:04:17 +0430

    Create AddToList.tsx

---
+++ src/components/AddToList.tsx
@@ -0,0 +1,80 @@
+import React, { HtmlHTMLAttributes, useState } from "react";
+import { IState as Props } from "../App";
+
+interface IProps {
+  people: Props["people"];
+  setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>;
+}
+const AddToList: React.FC<IProps> = ({ people, setPeople }) => {
+  const [input, setInput] = useState({
+    name: "",
+    age: "",
+    note: "",
+    img: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = (): void => {
+    if (!input.name || !input.age || !input.img) {
+      return;
+    }
+    setPeople([
+      ...people,
+      {
+        name: input.name,
+        age: parseInt(input.age),
+        url: input.img,
+        note: input.note,
+      },
+    ]);
+  };
+
+  return (
+    <div className="AddToList">
+      <input
+        type="text"
+        placeholder="Name"
+        className="AddToList-input"
+        value={input.name}
+        onChange={handleChange}
+        name="name"
+      />
+      <input
+        type="text"
+        placeholder="Age"
+        className="AddToList-input"
+        value={input.age}
+        name="age"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Image"
+        className="AddToList-input"
+        value={input.img}
+        name="img"
+        onChange={handleChange}
+      />
+      <textarea
+        placeholder="Note"
+        className="AddToList-input"
+        value={input.note}
+        onChange={handleChange}
+        name="note"
+      />
+      <button className="AddToList-btn" onClick={handleClick}>
+        Add To Team
+      </button>
+    </div>
+  );
+};
+
+export default AddToList;
